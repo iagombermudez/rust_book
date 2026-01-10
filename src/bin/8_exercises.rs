4ca2803279fa440fb617c8a0a63bd0ee4d8b51f8
Iago Bermúdez Milleiro <iagombermudez@gmail.com>
2026-01-10 22:43:57 +0100

    Add exercise 8.2

--- src/bin/8_exercises.rs
+++ src/bin/8_exercises.rs
@@ -26,4 +26,32 @@ fn main() {
         }
         None => println!("No mode found"),
     }
+
+    // Convert strings to Pig Latin. The first consonant of each word is moved to
+    // the end of the word and ay is added, so first becomes irst-fay.
+    // Words that start with a vowel have hay added to the end instead
+    // (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
+    //
+
+    fn ip_latinpay_ordway(word: &mut String) -> String {
+        let first_char = word.remove(0);
+        let vowels = "aeiouAEIOU";
+        if vowels.contains(first_char) {
+            word.insert(0, first_char);
+            word.push_str("-hay");
+        } else {
+            word.push_str("-");
+            word.push(first_char);
+            word.push_str("ay");
+        }
+        word.to_string()
+    }
+
+    let mut word = String::from("first");
+    let piglatin = ip_latinpay_ordway(&mut word);
+    println!("{piglatin}");
+
+    let mut word = String::from("apple");
+    let piglatin = ip_latinpay_ordway(&mut word);
+    println!("{piglatin}");
 }
